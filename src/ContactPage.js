5bd56d568b5d21a9477613c41906a1b30b9e7af9
Mayank Sharma <82099885+codersharma2001@users.noreply.github.com>
2023-06-22 19:56:59 +0530

    Update ContactPage.js

--- src/ContactPage.js
+++ src/ContactPage.js
@@ -2,10 +2,23 @@ import React from 'react';
 
 const ContactPage = () => {
   return (
-    <div>
-      <h2>Contact</h2>
-      <p>This is the Contact page.</p>
-    </div>
+    <section className="box" id="home">
+            {/* <h1>Welcome to SEVA Token</h1> */}
+            <p>Email :  https://www.web3nest.com/</p>
+            <p>Contact Us : +919304125236</p>
+        <section><div className="contact-form">
+        <input type="text" placeholder="Your Name" />
+        <input type="email" placeholder="Your Email" />
+        <textarea placeholder="Message"></textarea>
+        <button>Send</button>
+        
+             </div>
+             </section>
+          </section>
+
+      
+
+          
   );
 };
 
